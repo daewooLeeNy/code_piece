421d6b265df00b43bc75c7d9c1ad7b11e6a858b8
이대우(Lee Daewoo)/Syrup Wallet개발팀/SKP <daewoo.lee.ny@sk.com>
2017-08-03 13:17:07 +0900

    code_reader 추가

---
+++ code_reader.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+class CodeReader():
+    filename = 'korea_stock_code.csv'
+    data = None
+
+    @classmethod
+    def load(self, filename=None):
+        if filename is None:
+            filename = self.filename
+
+        ## code 값에 leading 0 유지 후 index 처리
+        self.data = pd.read_csv(filename, comment='#', converters={'code': lambda x: str(x)})
+        self.data = self.data.set_index('code')
+        return self.data;
+
+    @classmethod
+    def search_code(self, company):
+        if self.data is None:
+            self.load()
+
+        return self.data[self.data['company'].str.contains(company)]
+
+
+if __name__ == "__main__":
+    data = CodeReader().search_code('삼성전자');
+    print(data);
